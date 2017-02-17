a998fb9f4885778cce035660966071ca653256fe
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-17 22:11:03 +0100

    Post page created

--- app/js/app.js
+++ app/js/app.js
@@ -3,15 +3,15 @@ var notice = new Firebase('https://noticeboard-8dd7f.firebaseio.com/');
 
 function saveToList(event) {
     if (event.which == 13 || event.keyCode == 13) { // as the user presses the enter key, we will attempt to save the data
-        var notice = document.getElementById('notice').value.trim();
-        var group = document.getElementById('group').value.trim();
+        var notice = document.getByClassName('notice').value.trim();
+        var group = document.getElementById('dropdownMenuButton').value.trim();
         if (notice.length > 0 && group.length > 0 ) {
         	var li1 = '<li>' + notice + '</li>';
             var li2 = '<li>' + group + '</li>';
             document.getElementById('favMovies').innerHTML += li;
         }
-        document.getElementById('notice').value = '';
-        document.getElementById('group').value = '';
+        document.getElementByClassName('notice').value = '';
+        document.getElementById('dropdownMenuButton').value = '';
         return false;
     }
 }

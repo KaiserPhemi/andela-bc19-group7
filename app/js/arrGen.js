c177b54f74cbcd026696fb2e503d218e4d5a1132
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-18 19:20:07 +0100

    New JS file added, second html page modified

---
+++ app/js/arrGen.js
@@ -0,0 +1,30 @@
+(function(){
+	
+	'use strict';
+
+	var generateTable = function(){
+
+		var count 	=0,
+			content = {
+				recipient:"",
+				title:"",
+				messageBody:""
+			},
+			colCount = Object.keys(content).length;
+		var table = document.getElementsByClassName("dash-board");
+		
+		var row,
+			content.recipient = document.getElementsByClassName("recipent"),
+			content.title = document.getElementsByClassName("notice-title"),
+			content.messageBody = document.getElementsByClassName("message-body");
+		
+		while(count < colCount){
+			row = table.insertRow(-),
+			var cell = row.insertCell(-1);
+			Object.entries(content).forEach{
+				cell.innerHTML = value;	
+			}
+			 count++;
+		}
+	};
+})();
